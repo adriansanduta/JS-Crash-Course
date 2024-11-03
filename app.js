ec14737b26e1cd61b53d711e6b998af9a33ed862
Adrian <adriansanduta@gmail.com>
2024-11-03 17:07:21 -0600

    if else

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 /* CONVERTING TEMPERATURES */
 
 let celsius = 10;
-let fahrenheit = celsius * 1.8 + 32
+let fahrenheit = celsius * 1.8 + 32;
 console.log (fahrenheit);
+
+/* If else */
+
+let subscribed = true
+if (subscribed === true ) {
+    console.log('show the video')
+}
+else {
+    console.log('tell user to log into account')
+}
