8ef3e9ffdd2b0b0c30e23ae9b03f64f282110fd1
Adrian <adriansanduta@gmail.com>
2024-11-03 17:09:53 -0600

    if , else if, else

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ console.log (fahrenheit);
 
 /* If else */
 
-let subscribed = true
+let subscribed = false
+let loggedIn = true
 if (subscribed === true ) {
     console.log('show the video')
+}
+else if (loggedIn === true) {
+    console.log('tell the user to upgrade their subscription')
 }
 else {
     console.log('tell user to log into account')
