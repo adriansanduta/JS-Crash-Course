ef17ac4817dfd21693bee7748a1f713fbd1c9141
Adrian <adriansanduta@gmail.com>
2024-11-21 19:34:38 -0600

    callback shorter

--- app.js
+++ app.js
@@ -199,8 +199,6 @@ console.log(arr);
 
 let arr = [20, 30, 40, 50, 100]
 
-let newArr = arr.filter(element => {
-    return element < 50 
-})
+let newArr = arr.filter(element => element < 50 )
 console.log(newArr)
 
