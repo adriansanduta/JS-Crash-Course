700ef6c0f413bde419af01736cdd37117734a942
Adrian <adriansanduta@gmail.com>
2024-11-21 19:27:03 -0600

    callback

--- app.js
+++ app.js
@@ -183,3 +183,24 @@ console.log(arr[0])
 
 // Last elemnt of array:
 console.log(arr[arr.lenght - 1])
+
+// Add element onto end of array:
+arr.push(200)
+
+// Filter (callback function):
+arr.filter((element) => {
+    console.log(element)
+    return true;
+})
+
+console.log(arr);
+
+// Shorter version:
+
+let arr = [20, 30, 40, 50, 100]
+
+let newArr = arr.filter(element => {
+    return element < 50 
+})
+console.log(newArr)
+
