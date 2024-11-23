b9d750773d954d7a3062ed58d1164d53eee9c589
Adrian <adriansanduta@gmail.com>
2024-11-23 17:37:12 -0600

    Turn each element in an array of dollars into cents

--- app.js
+++ app.js
@@ -233,3 +233,24 @@ let arr = [1, 4, 9, 16]
 arr.map((element) => {
     console.log(element)
 })
+
+/**
+ * Turn each element in an array of dollars into cents
+ * 
+ * @examples
+ * [1, 5, 10, 3] => [100, 500, 1000, 300]
+ * [0, 10, 20] => [0, 1000, 2000]
+ */
+
+let dollars = [1, 5, 10, 3]
+let cents = dollars.map(element => element * 100);
+console.log(cents);
+
+/**
+ * Turn each element in an array of dollars into cents
+ * without map method
+ * 
+ * @examples
+ * [1, 5, 10, 3] => [100, 500, 1000, 300]
+ * [0, 10, 20] => [0, 1000, 2000]
+ */
